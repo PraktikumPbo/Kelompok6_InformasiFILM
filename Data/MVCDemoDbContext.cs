37533b2115a65031527824ee88a3a54fa2aa848f
FarhanRizkiM <110893795+FarhanRizkiM@users.noreply.github.com>
2023-01-16 20:28:41 +0700

    Add files via upload

---
+++ Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ASPFILM.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPFILM.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Film> Films { get; set; }
+    }
+}
